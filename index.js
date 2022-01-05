09246c131993859163c6946e4888c971a2e33adc
Jason Wisniewski <jwizinpa95@gmail.com>
2022-01-04 22:25:42 -0700

    connecting inquirer and jest

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const inquirer = require('inquirer');
+
+Assignment.prototype.startApp = function(){
+  console.log('hey buddy');
+  inquirer
+  .prompt({
+    type: 'text',
+    name: 'name',
+    message: 'What is your name?'
+  })
+  .then(({ name }) => {
+    this.player = new Player(name);
+
+    this.startNewBattle();
+  });
+};
+
